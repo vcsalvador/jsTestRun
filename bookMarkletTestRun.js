63746613e27ac96003875bc4ba0c76d5026708cb
Victor <victorcsalvador@gmail.com>
2015-09-07 20:45:06 +0000

    organized and functional

--- bookMarkletTestRun.js
+++ bookMarkletTestRun.js
@@ -1,34 +1,56 @@
-var GroupingCriteria = function(ruleList){
-    this.rules = Array.from(ruleList).filter(function(element){
-        return element.type == 1;
-    });
+GroupingCriteria = function(ruleList){
+    
+    this.rules = Array.from(ruleList).filter((element) => element.type == 1);
     this.weight = 1;
-    this.resultList = [];
-    this.calculateRule = function(rules){
-
-    };
+    
+    this.resultList = this.rules.map((currentValue) => 
+        new Result(currentValue, calculateRule(currentValue, this.weight)));
+    
+    function calculateRule(rule, weight){
+        return (rule.selectorText.split(',').length - 1) * weight;
+    }
 };
 
-var Result = function(){
+RareSelectorsCriteria = function(ruleList){
+    
+    this.pattern = /(\[{1}.*=.*\]{1})|(>|~|\+)/g;
+    this.rules = Array.from(ruleList).filter((element) => element.type == 1);
+    this.weight = 3;
+    
+    this.resultList = this.rules.map((currentValue) => 
+        new Result(currentValue, calculateRule(currentValue, this.weight)));
+    
+    function calculateRule(rule, weight){
+        var count = 0;
+        while ((m = pattern.exec(rule.selectorText)) !== null) {
+            if (m.index === pattern.lastIndex) {
+                count++;
+                pattern.lastIndex++;
+            }
+        }
+        return count*weight
+    }
+};
+
+var Result = function(cssRule, score){
     this.cssRule;
     this.score;
-    this.htmlMatches = function(){
-        this.cssRule.selectorText
-    }
+    this.htmlMatches = () => this.cssRule.selectorText;
 };
 
 var myStyleSheet = function(styleSheet){
     this.styleSheet = styleSheet;
-    this.groupingCriteria = new GroupingCriteria(styleSheet.cssRules);
-
+    this.criteriaList = [];
+    this.criteriaList.push(styleSheet.cssRules ? new GroupingCriteria(styleSheet.cssRules):null);
+    this.cirteriaList.push(styleSheet.cssRules ? new RareSelectorsCriteria(styleSheet.cssRules):null);
 };
 
-function(){
+() => {
     var arr = Array.from(document.styleSheets);
 
-    var styleList = arr.map(function(currentValue){
+    var myStyleSheetList = arr.map(function(currentValue){
         return new myStyleSheet(currentValue);
     });
 
-    console.log(styleList);
+    console.log(myStyleSheetList);
 }();
